18bfa842f3fd8d8bdf3314ec8d2d99b84c7a83fc
1qwert19 <so.you.best74.33@gmail.com>
2024-10-08 22:57:34 +0300

    commit

---
+++ muratovlab0HW/muratovlab0HW/muratovlab0HW.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+
+    char inputChar;
+    std::cout << "input: ";
+    std::cin >> inputChar;
+
+    std::cout << "output: " << inputChar << std::endl;
+
+    return 0;
+}
